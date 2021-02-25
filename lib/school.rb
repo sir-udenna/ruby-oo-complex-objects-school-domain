c05b8e72f99f84ebb4daabfb5586f8613f2751ee
sir-udenna <udennaboy@gmail.com>
2021-02-25 00:55:06 -0600

    commit tree e6ba3e64de1343955d8386ecf638d152da9d4e1b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << "#{name}"
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        # binding.pry
+        @roster.sort
+
+        @roster.each do |grade, students|
+            @roster[grade].sort
+        end
+    end
+end
